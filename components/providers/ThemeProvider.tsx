abc8e0bbfac8df2819fd6e16d36a4e57b5d51c57
Nikhilkushwaa <nikhik15kumar@gmail.com>
2025-02-24 03:59:00 +0530

    first commit

--- components/providers/ThemeProvider.tsx
+++ components/providers/ThemeProvider.tsx
@@ -1,6 +1,14 @@
 'use client';
 
-import { ThemeProvider as NextThemesProvider, ThemeProviderProps } from 'next-themes';
+import { ThemeProvider as NextThemesProvider } from 'next-themes';
+import { ReactNode } from 'react';  // Import ReactNode for type safety
+
+interface ThemeProviderProps {
+  children: ReactNode;  // Explicitly type children as ReactNode
+  // You can extend this with any other props if needed, e.g.:
+  // attribute?: string;
+  // enableSystem?: boolean;
+}
 
 export default function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
